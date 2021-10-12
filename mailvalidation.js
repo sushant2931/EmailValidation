d5a753c1d8110af9fd8442fc14d98f346b0d6a81
sushant2931 <sushantlad8888@gmail.com>
2021-10-12 23:56:50 +0530

    ADDED : Using Regex Solve Email Validation

---
+++ mailvalidation.js
@@ -0,0 +1,87 @@
+console.log("***** Welcome To Email Validation *****");
+
+const emailRegex = RegExp("^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$");
+
+function validateEmail(email) {
+    if (emailRegex.test(email))
+        console.log(email + " : Valid EMAIL !!");
+    else
+        console.log(email + " : EMAIL is Invalid !");
+}
+
+console.log("---------- Valid Emails ----------");
+		
+let email = "abc@bridgelabz.co.in";
+validateEmail(email);
+
+email = "abc@gmail.com.com";
+validateEmail(email);
+
+email = "abc@yahoo.com";
+validateEmail(email);
+
+email = "abc@1.com";
+validateEmail(email);
+
+email = "abc-100@yahoo.com";
+validateEmail(email);
+
+email = "abc.100@yahoo.com";
+validateEmail(email);
+
+email = "abc111@abc.com";
+validateEmail(email);
+
+email = "abc-100@abc.net";
+validateEmail(email);
+
+email = "abc.100@abc.com.au";
+validateEmail(email);
+
+email = "abc+100@gmail.com";
+validateEmail(email);
+
+console.log("---------------------------------------------------");
+
+console.log("---------- Invalid Emails ----------");
+
+email = ".abc@abc.com";
+validateEmail(email);
+
+email = "abc";
+validateEmail(email);
+
+email = "abc@.com.my";
+validateEmail(email);
+
+email = "abc@abc@gmail.com";
+validateEmail(email);
+
+email = "abc()*@gmail.com";
+validateEmail(email);
+
+email = "abc..2002@gmail.com";
+validateEmail(email);
+
+email = "abc.@gmail.com";
+validateEmail(email);
+
+email = "abc123@.com";
+validateEmail(email);
+
+email = "abc123@.com.com";
+validateEmail(email);
+
+email = "abc123@gmail.a";
+validateEmail(email);
+
+email = "abc@%*.com";
+validateEmail(email);
+
+email = "abc@gmail.com.1a";
+validateEmail(email);
+
+email = "abc@gmail.com.aa.au";
+validateEmail(email);
+
+console.log("---------------------------------------------------");
